8ef09b2ee1e90f7ad859db004c915ea512b612b1
Bukiyo96 <alexbolbo74@gmail.com>
2023-03-22 20:57:23 +0200

    Am adaugat baza de date ca tema de acasa

---
+++ baza_de_date.py
@@ -0,0 +1,123 @@
+
+
+
+
+
+elev1 = { "Nume" : "Bolbocean",
+    "Prenume" : "Alexandru",
+    "Disciplina1" : "Fizica",
+    "Disciplina2 "  : " Engleza",
+    "Disciplina3" : " Matematica",
+    "Disciplina4" : "Romana",
+    "Nota1" : [5,4,10,2,7],
+    "Nota2" : [10,9,10,5,8],
+    "Nota3" : [8,7,9,4,5],
+    "Nota4" : [5,7,3,8,9]
+}
+
+n = "Nota1"
+n2 = "Nota2"
+n3 = "Nota3"
+n4 = "Nota4"
+
+media_notelor1 = sum(elev1[n]) / len(elev1[n])
+media_notelor2 = sum(elev1[n2]) / len(elev1[n2])
+media_notelor3 = sum(elev1[n3]) / len(elev1[n3])
+media_notelor4 = sum(elev1[n4]) / len(elev1[n4])
+
+
+
+print("Nota medie al elevului ", elev1["Nume"], elev1["Prenume"], " la obiectul ", elev1["Disciplina1"], "este " , media_notelor1)
+print("Nota medie al elevului ", elev1["Nume"], elev1["Prenume"], " la obiectul ", elev1["Disciplina2 "], "este " , media_notelor2)
+print("Nota medie al elevului ", elev1["Nume"], elev1["Prenume"], " la obiectul ", elev1["Disciplina3"], "este " , media_notelor3)
+print("Nota medie al elevului ", elev1["Nume"], elev1["Prenume"], " la obiectul ", elev1["Disciplina4"], "este " , media_notelor4)
+
+
+
+elev2 = { "Nume" : "Branza",
+    "Prenume" : "Daniel",
+    "Disciplina1" : "Fizica",
+    "Disciplina2 "  : " Engleza",
+    "Disciplina3" : " Matematica",
+    "Disciplina4" : "Romana",
+    "Nota5" : [9,2,7,3,6],
+    "Nota6" : [5,9,4,5,8],
+    "Nota7" : [6,7,2,4,6],
+    "Nota8" : [5,7,8,5,9]
+}
+
+n5 = "Nota5"
+n6 = "Nota6"
+n7 = "Nota7"
+n8 = "Nota8"
+
+media_notelor5 = sum(elev2[n5]) / len(elev2[n5])
+media_notelor6 = sum(elev2[n6]) / len(elev2[n6])
+media_notelor7 = sum(elev2[n7]) / len(elev2[n7])
+media_notelor8 = sum(elev2[n8]) / len(elev2[n8])
+
+
+
+print("Nota medie al elevului ", elev2["Nume"], elev2["Prenume"], " la obiectul ", elev2["Disciplina1"], "este " , media_notelor5)
+print("Nota medie al elevului ", elev2["Nume"], elev2["Prenume"], " la obiectul ", elev2["Disciplina2 "], "este " , media_notelor6)
+print("Nota medie al elevului ", elev2["Nume"], elev2["Prenume"], " la obiectul ", elev2["Disciplina3"], "este " , media_notelor7)
+print("Nota medie al elevului ", elev2["Nume"], elev2["Prenume"], " la obiectul ", elev2["Disciplina4"], "este " , media_notelor8)
+
+elev3 = { "Nume" : "Alina",
+    "Prenume" : "Matei",
+    "Disciplina1" : "Fizica",
+    "Disciplina2 "  : " Engleza",
+    "Disciplina3" : " Matematica",
+    "Disciplina4" : "Romana",
+    "Nota9" : [8,7,6,5,8],
+    "Nota10" : [9,6,9,6,9],
+    "Nota11" : [5,4,7,6,8],
+    "Nota12" : [2,3,5,10,10]
+}
+
+n9 = "Nota9"
+n10 = "Nota10"
+n11 = "Nota11"
+n12 = "Nota12"
+
+media_notelor9 = sum(elev3[n9]) / len(elev3[n9])
+media_notelor10 = sum(elev3[n10]) / len(elev3[n10])
+media_notelor11 = sum(elev3[n11]) / len(elev3[n11])
+media_notelor12 = sum(elev3[n12]) / len(elev3[n12])
+
+
+
+print("Nota medie al elevului ", elev3["Nume"], elev3["Prenume"], " la obiectul ", elev3["Disciplina1"], "este " , media_notelor9)
+print("Nota medie al elevului ", elev3["Nume"], elev3["Prenume"], " la obiectul ", elev3["Disciplina2 "], "este " , media_notelor10)
+print("Nota medie al elevului ", elev3["Nume"], elev3["Prenume"], " la obiectul ", elev3["Disciplina3"], "este " , media_notelor11)
+print("Nota medie al elevului ", elev3["Nume"], elev3["Prenume"], " la obiectul ", elev3["Disciplina4"], "este " , media_notelor12)
+
+
+elev4 = { "Nume" : "Pontea",
+    "Prenume" : "Pavel",
+    "Disciplina1" : "Fizica",
+    "Disciplina2 "  : " Engleza",
+    "Disciplina3" : " Matematica",
+    "Disciplina4" : "Romana",
+    "Nota13" : [10,9,8,10,5],
+    "Nota14" : [4,8,6,8,3],
+    "Nota15" : [7,8,5,2,10],
+    "Nota16" : [7,6,8,5,10]
+}
+
+n13 = "Nota13"
+n14 = "Nota14"
+n15 = "Nota15"
+n16 = "Nota16"
+
+media_notelor13 = sum(elev4[n13]) / len(elev4[n13])
+media_notelor14 = sum(elev4[n14]) / len(elev4[n14])
+media_notelor15 = sum(elev4[n15]) / len(elev4[n15])
+media_notelor16 = sum(elev4[n16]) / len(elev4[n16])
+
+
+
+print("Nota medie al elevului ", elev4["Nume"], elev4["Prenume"], " la obiectul ", elev4["Disciplina1"], "este " , media_notelor13)
+print("Nota medie al elevului ", elev4["Nume"], elev4["Prenume"], " la obiectul ", elev4["Disciplina2 "], "este " , media_notelor14)
+print("Nota medie al elevului ", elev4["Nume"], elev4["Prenume"], " la obiectul ", elev4["Disciplina3"], "este " , media_notelor15)
+print("Nota medie al elevului ", elev4["Nume"], elev4["Prenume"], " la obiectul ", elev4["Disciplina4"], "este " , media_notelor16)
