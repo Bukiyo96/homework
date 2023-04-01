d797347a8f684e8e92310c0228c0b8e38d7462e4
Bukiyo96 <alexbolbo74@gmail.com>
2023-04-01 10:40:08 +0300

    Am adaugat baza de date ca tema de acasa

--- Tema_pe_acasa_registru.py
+++ Tema_pe_acasa_registru.py
@@ -1,71 +1,42 @@
 
-
-
-registru = []
-registru1 = []
-registru2 = []
-registru3 = []
+register = []
 student = {}
-student1 = {}
-student2 = {}
-student3 = {}
+# Read data from user
+st_num = int(input("How Many Students do you want to add? "))
+sub_num = int(input("How many Subjects they have? "))
+
+# For cycle
+for j in range(0, st_num):
+    # Read names
+    s_name = input("Name: ")
+    student.update({"Name": s_name})
+    suma = 0
+    for i in range(0, sub_num):
+        # Read data about subj and mark
+        subject = input("Subject: ")
+        mark = float(input("Mark: "))
+
+        # Add this data to dictionary
+        student.update({subject: mark})
+        suma += mark
+        #
+    media = suma / sub_num
+    student.update({"Media": media})
+    # Add students to register
+
+    register.append(student.copy())
+#Print register
+print(register)
 
 
-nr_studenti = int(input("Cati studenti doresti sa adaugi?:"))
-nr_obiecte = int(input("Cate obiecte doresti sa adaugi?:"))
 
 
-for s in range(0, nr_studenti):
-    nume_student =input("Nume ")
-    student.update({"Name " : nume_student})
-
-    for o in range(0, nr_obiecte):
-        obiect = input("Obiect ")
-        nota = int(input("Nota "))
-
-        student.update({obiect: nota})
-
-    registru.append(student)
 
 
-for s in range(0, nr_studenti):
-    nume_student =input("Nume ")
-    student1.update({"Name " : nume_student})
 
-    for o in range(0, nr_obiecte):
-        obiect = input("Obiect ")
-        nota = int(input("Nota "))
 
-        student1.update({obiect: nota})
 
-    registru1.append(student1)
 
-for s in range(0, nr_studenti):
-     nume_student = input("Nume ")
-     student2.update({"Name ": nume_student})
 
-     for o in range(0, nr_obiecte):
-        obiect = input("Obiect ")
-        nota = int(input("Nota "))
 
-        student2.update({obiect: nota})
-
-     registru2.append(student2)
-
-for s in range(0, nr_studenti):
-    nume_student =input("Nume ")
-    student3.update({"Name " : nume_student})
-
-    for o in range(0, nr_obiecte):
-        obiect = input("Obiect ")
-        nota = int(input("Nota "))
-
-        student3.update({obiect: nota})
-
-    registru3.append(student3)
-
-print(registru)
-print(registru1)
-print(registru2)
-print(registru3)
 
